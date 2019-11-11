02df0e563624312a756cadf7c86e82bbe94588d1
Andre <andre.rian@inventsoftware.com.br>
2019-11-11 17:31:36 -0300

    adicionar o firebase ao projeto

--- app/src/main/java/com/example/chatfirebase/CadastroActivity.java
+++ app/src/main/java/com/example/chatfirebase/CadastroActivity.java
@@ -3,12 +3,30 @@ package com.example.chatfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private EditText editEmail, editSenha;
+    private Button btnCadastra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        editEmail = findViewById(R.id.edit_email);
+        editSenha = findViewById(R.id.edit_senha);
+        btnCadastra = findViewById(R.id.btn_cadastra);
+
+        btnCadastra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Teste", editEmail.getText().toString());
+                Log.d("Teste", editSenha.getText().toString());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/chatfirebase/MainActivity.java
+++ app/src/main/java/com/example/chatfirebase/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.chatfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 
+            }
+        });
+
+        cadastraConta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(intent);
             }
         });
     }
